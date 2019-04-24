0ab6b6a630fb6d45a380d8ca8924fa0e1fbe7695
acdall <acdall@gmail.com>
2019-04-24 14:55:22 -0700

    remove login as close

--- Login.cs
+++ Login.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 //test1234
 namespace Audit_System
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
         }
--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace Audit_System
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }

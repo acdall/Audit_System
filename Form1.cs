5203dc914e2cbc468f44dcbab6ebbff520a63c14
acdall <acdall@gmail.com>
2019-04-24 12:49:59 -0700

    login button code
    needs to be changed to login, currently set to close program

--- Form1.cs
+++ Form1.cs
@@ -21,5 +21,10 @@ namespace Audit_System
         {
             this.Close();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
